9460d02902eb912e50cf61615ec48ebe2d29187c
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2022-02-19 18:19:14 +0530

    Update nad_j7elte.mk

--- nad_j7elte.mk
+++ nad_j7elte.mk
@@ -20,8 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit common Lineage phone.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-WITH_GMS_MINIMAL := true
+$(call inherit-product, vendor/nad/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := nad_j7elte
