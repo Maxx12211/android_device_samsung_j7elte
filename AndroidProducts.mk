84d36ae8fefac4ec0638bafe36910266faf11eb6
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2022-02-20 20:29:28 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/nad_j7elte.mk
+    $(LOCAL_PATH)/aosip_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_j7elte-user \
-    aosp_j7elte-userdebug \
-    aosp_j7elte-eng
+    aosip_j7elte-user \
+    aosip_j7elte-userdebug \
+    aosip_j7elte-eng
