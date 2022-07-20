c73b3e6162bc4dd73cfff54eb91a5bf00ec79691
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2022-07-20 15:36:33 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/pixys_j7elte.mk
+    $(LOCAL_PATH)/lineage_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixys_j7elte-user \
-    pixys_j7elte-userdebug \
-    pixys_j7elte-eng
+    lineage_j7elte-user \
+    lineage_j7elte-userdebug \
+    lineage_j7elte-eng
