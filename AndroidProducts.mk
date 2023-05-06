5f1fe1670c8d4df826952227c26e35eb9b276d0f
Maxx12211 <somanathcharan9@gmail.com>
2023-05-06 17:25:36 +0530

    j7elte : nusantara bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/aosp_j7elte.mk
+    $(LOCAL_PATH)/nad_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_j7elte-user \
-    aosp_j7elte-userdebug \
-    aosp_j7elte-eng
+    nad_j7elte-user \
+    nad_j7elte-userdebug \
+    nad_j7elte-eng
